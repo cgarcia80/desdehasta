aaa29434aa5857f74cc9f7a383cc9e5df1f19776
cesar <cesar.osc.garcia@gmail.com>
2021-09-27 22:29:46 -0300

    Dibujando la ruta en el mapa

--- app/src/main/java/com/ar/desdehasta/MainActivity.kt
+++ app/src/main/java/com/ar/desdehasta/MainActivity.kt
@@ -2,16 +2,23 @@ package com.ar.desdehasta
 
 import android.app.Activity
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.AutoCompleteTextView
-import android.widget.TextView
+import com.akexorcist.googledirection.GoogleDirection
+import com.akexorcist.googledirection.config.GoogleDirectionConfiguration
+import com.akexorcist.googledirection.constant.TransportMode
+import com.akexorcist.googledirection.model.Direction
+import com.akexorcist.googledirection.model.Route
+import com.akexorcist.googledirection.util.DirectionConverter
+import com.akexorcist.googledirection.util.execute
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.libraries.places.api.Places
@@ -19,6 +26,7 @@ import com.google.android.libraries.places.api.model.Place
 import com.google.android.libraries.places.widget.Autocomplete
 import com.google.android.libraries.places.widget.AutocompleteActivity
 import com.google.android.libraries.places.widget.model.AutocompleteActivityMode
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -28,7 +36,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         private const val TO_REQUEST_CODE_AUTOCOMPLETE = 2
         private const val TAG = "MainActivity"
     }
-    private lateinit var mMap: GoogleMap
+    private lateinit var googleMap: GoogleMap
     private lateinit var mOrigenLatLng: LatLng
     private lateinit var mDestinoLatLng: LatLng
     private var mMarkerOrigen: Marker? = null
@@ -90,6 +98,17 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                     mDestinoLatLng=it
                     setMarkerTo(mDestinoLatLng)
 
+
+                    GoogleDirectionConfiguration.getInstance().isLogEnabled = BuildConfig.DEBUG
+                    GoogleDirection.withServerKey(getString(R.string.api_key))
+                        .from(mOrigenLatLng)
+                        .to(mDestinoLatLng)
+                        .transportMode(TransportMode.BICYCLING)
+                        .execute(
+                            onDirectionSuccess = { direction -> onDirectionSuccess(direction) },
+                            onDirectionFailure = { t -> onDirectionFailure(t) }
+                        )
+
                 }
             }
             return
@@ -122,10 +141,10 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
+        this.googleMap = googleMap
         //zoom
-        mMap.setMinZoomPreference(15f)
-        mMap.setMaxZoomPreference(20f)
+        this.googleMap.setMinZoomPreference(15f)
+        this.googleMap.setMaxZoomPreference(20f)
 
         // Add a marker in Sydney and move the camera
         //val ortBelgrano = LatLng(-34.5497773,-58.4541588)
@@ -138,14 +157,15 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             val markerOptions=MarkerOptions()
                 .position(latLng)
                 .title(title)
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng))
 
-        return mMap.addMarker(markerOptions)
+        return googleMap.addMarker(markerOptions)
 
     }
     private fun setMarkerFrom(latLng: LatLng){
-        mMarkerDestino?.remove()
 
+        mMarkerOrigen?.remove()
+        mMarkerDestino?.remove()
         mMarkerOrigen= addMarker(latLng,getString(R.string.macador_origen))
 
     }
@@ -154,5 +174,50 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
         mMarkerDestino= addMarker(latLng,getString(R.string.macador_origen))
 
+    }
+
+
+    private fun onDirectionSuccess(direction: Direction?) {
+        direction?.let {
+           // showSnackbar(getString(R.string.success_with_status, direction.status))
+            if (direction.isOK) {
+                val route = direction.routeList[0]
+                mMarkerOrigen?.remove()
+                googleMap?.addMarker(MarkerOptions().position(mOrigenLatLng))
+                mMarkerDestino?.remove()
+                googleMap?.addMarker(MarkerOptions().position(mDestinoLatLng))
+                val directionPositionList = route.legList[0].directionPoint
+                googleMap?.addPolyline(
+                    DirectionConverter.createPolyline(
+                        this,
+                        directionPositionList,
+                        5,
+                        Color.RED
+                    )
+                )
+                setCameraWithCoordinationBounds(route)
+                //binding.buttonRequestDirection.visibility = View.GONE
+            } else {
+                showSnackbar(direction.status)
+            }
+        } ?: run {
+            showSnackbar(getString(R.string.success_with_empty))
+        }
+    }
+
+    private fun onDirectionFailure(t: Throwable) {
+        showSnackbar(t.message)
+    }
+    private fun setCameraWithCoordinationBounds(route: Route) {
+        val southwest = route.bound.southwestCoordination.coordination
+        val northeast = route.bound.northeastCoordination.coordination
+        val bounds = LatLngBounds(southwest, northeast)
+        googleMap?.animateCamera(CameraUpdateFactory.newLatLngBounds(bounds, 100))
+    }
+
+    private fun showSnackbar(message: String?) {
+        message?.let {
+            Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_SHORT).show()
+        }
     }
 }
